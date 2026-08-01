3d1bb2cdd6da8b35bb5b840bc3fa9c2fccf1131c
SangPY <dothanhsang95@gmail.com>
2026-08-02 00:11:50 +0700

    Secure API with JSON Web Tokens & Refresh Tokens

---
+++ dts_login_service/Controllers/AuthController.cs
@@ -0,0 +1,31 @@
+﻿using dts_login_service.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace dts_login_service.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private readonly IConfiguration _config;
+
+        public AuthController(IConfiguration config)
+        {
+            _config = config;
+        }
+
+        [HttpPost("login")]
+        public IActionResult Login([FromBody] User user)
+        {
+            if (user.Email == "sangdt@gmail.com"
+                && user.Password == "Sa@123")
+            {
+                var token = JwtTokenService.GenerateToken(user.Name, _config);
+                return Ok(new { Token = token });
+            }
+
+            return Unauthorized("Invalid credentials");
+        }
+    }
+}
---
+++ dts_login_service/Services/JwtTokenService.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace dts_login_service.Services
+{
+    public static class JwtTokenService
+    {
+        public static string GenerateToken(string username, IConfiguration config)
+        {
+            var securityKey = new SymmetricSecurityKey(
+                Encoding.UTF8.GetBytes(config["Jwt:Key"]));
+            var credentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
+            var claims = new[]
+            {
+            new Claim(JwtRegisteredClaimNames.Name, username),
+            //new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString())
+        };
+            var token = new JwtSecurityToken(
+                issuer: config["Jwt:Issuer"],
+                audience: config["Jwt:Audience"],
+                claims: claims,
+                expires: DateTime.UtcNow.AddMinutes(
+                    config.GetValue<int>("Jwt:ExpiresInMinutes", 60)),
+                signingCredentials: credentials
+            );
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
+    }
+}
