de9ad086c72796cd268994210b65d53090200f52
SangPY <dothanhsang95@gmail.com>
2026-08-10 15:15:22 +0700

    Edit router | Project        |    HTTPS | | -------------- | -------: | | Ocelot Gateway | **7100** | | YARP Gateway   | **7200** | | Catalog        | **7301** | | Order          | **7302** | | Login          | **7303** | | User           | **7304** |

---
+++ dts_orders_service/Controllers/OrderController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace dts_orders_service.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OrderController : ControllerBase
+    {
+        [HttpGet("GetOrders")]
+        public IActionResult GetOrders()
+        {
+            return Ok(new[] { "Order 1", "Order 2", "Order 3" });
+        }
+
+    }
+}
